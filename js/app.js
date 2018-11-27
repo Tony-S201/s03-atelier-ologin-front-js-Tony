be78684ac202cc15167c78321e5640e5d5ab0c75
Tony Swierz <swierz.tony@gmail.com>
2018-11-27 12:23:44 +0100

    presque fini

--- js/app.js
+++ js/app.js
@@ -1,7 +1,50 @@
 var app = {
   init: function() {
-    console.log('app.init'); 
-  }
+    var form = document.querySelector('#login-form');
+    form.addEventListener('submit', app.getItem);
+  },
+
+  getItem : function(event){
+
+    event.preventDefault();
+    var items = app.getItemValue();
+    app.content;
+    
+    for (let j = 0; j < items.length; j += 1) {
+    
+       
+      if (items[j].value.length < 3) {
+        var message = "Le champ" + app.fields[j] + " doit faire plus de trois caractères.";
+        app.addItem(message);
+    
+      }else{
+            console.log('ça marche');
+      }
+
+    }
+  },
+      
+
+  getItemValue : function(){
+      var inputs = document.querySelectorAll('.field-input');
+
+      return inputs;
+  },
+
+  fields: [
+    'Identifiant',
+    'Mot de passe'
+  ],
+
+  addItem: function(messageError){
+      var divElement = document.querySelector('#errors');
+      var itemP = document.createElement('p');
+
+      itemP.textContent = messageError;
+      divElement.appendChild(itemP);
+  },
+
+
 };
 
 document.addEventListener('DOMContentLoaded', app.init);
